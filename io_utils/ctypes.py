0a613f629b0d480ebc860faef0fbc75f1c3f9f46
Skarn <skarnproject@gmail.com>
2022-04-19 11:00:56 +0300

    push changes to parsing library

--- io_utils/ctypes.py
+++ io_utils/ctypes.py
@@ -19,15 +19,14 @@ int8 = GenericType('b', int, 1)
 uint8 = GenericType('B', int, 1)
 int16 = GenericType('h', int, 2)
 uint16 = GenericType('H', int, 2)
-int32 = GenericType('i', bytes, 4)
-uint32 = GenericType('I', bytes, 4)
-int64 = GenericType('q', bytes, 8)
-uint64 = GenericType('Q', bytes, 8)
-float16 = GenericType('e', bytes, 2)
-float32 = GenericType('f', bytes, 4)
-float64 = GenericType('d', bytes, 8)
+int32 = GenericType('i', int, 4)
+uint32 = GenericType('I', int, 4)
+int64 = GenericType('q', int, 8)
+uint64 = GenericType('Q', int, 8)
+float16 = GenericType('e', float, 2)
+float32 = GenericType('f', float, 4)
+float64 = GenericType('d', float, 8)
 
 # aliases
 double = float64
 char = byte
-
--- io_utils/struct.py
+++ io_utils/struct.py
@@ -2,14 +2,51 @@ from .ctypes import *
 
 from io import IOBase
 from types import FunctionType
-from typing import Protocol, Self, runtime_checkable
+from typing import Protocol, List, Union, Tuple, Any, Optional, runtime_checkable
 
 
+class StructError(Exception):
+    pass
+
+T = List[Union[Tuple[str, str, Any, Optional[Any], int], 'T_f']]
 @runtime_checkable
 class StructIOProtocol(Protocol):
-    def write(self, f: IOBase) -> Self: ...
-    def read(self, f: IOBase) -> Self: ...
+    def write(self, f: IOBase) -> 'StructIOProtocol': ...
+    def read(self, f: IOBase) -> 'StructIOProtocol': ...
+
+    '''
+    @classmethod
     def size(self) -> int: ...
+
+    @classmethod
+    def is_pod(cls) -> bool: ...
+
+    @classmethod
+    def type_of(cls, name: str) -> Union[GenericType, 'StructIOProtocol']: ...
+    '''
+
+    @classmethod
+    def _struct_format_chunked(cls) -> T: ...
+
+
+def recurse_format_chunks(format_chunks: T) -> str:
+
+    for i, (attr_name, format, default_value, n_values) in enumerate(format_chunks):
+        if n_values == 0:
+            continue
+
+        is_trivial = not isinstance(format, list)
+
+        # handle basic data types
+        if is_trivial:
+            if n_values == 1:
+                yield format
+            else:
+                yield f"{n_values}{format}"
+        else:
+            for i in range(n_values):
+                yield from recurse_format_chunks(format)
+
 
 
 class StructMeta(type):
@@ -17,28 +54,58 @@ class StructMeta(type):
     def __new__(mcs, classname, bases, cls_dict):
         annotations = cls_dict.get('__annotations__')
         if annotations is None:
-            raise RuntimeError("Empty structs are not supported. A struct must contain data-type annotations.")
+            raise StructError(f"Struct <'{classname}'>: Empty structs are not supported. "
+             "A struct must contain data-type annotations.")
 
         is_plain_old_data = False
 
-        data_fields = {}
-        logic_fields = {}
+        format_chunks: T = []
 
         for attr_name, annotation_type in annotations.items():
 
             # skip fields that do not represent the data layout
-            if not isinstance(annotation_type, StructIOProtocol) or not isinstance(annotation_type, GenericType):
+            is_sub_struct = isinstance(annotation_type, StructIOProtocol)
+            if not is_sub_struct and not isinstance(annotation_type, GenericType):
                 continue
 
+            if is_sub_struct:
+                # handle nested structures
+                format_chunks.append((attr_name, annotation_type._struct_format_chunked(), None, 1))
+
+                if attr_name in cls_dict:
+                    raise StructError(f"Struct <'{classname}'>: Default values are only supported for basic data types. "
+                                      f"Field '{attr_name}' must not have a default value.")
 
 
-        for attr_name, attribute in cls_dict.items():
-            if isinstance(attribute, FunctionType) or (annotation_type := annotations.get(attr_name) is None):
-                continue
+            else:
+                # handle plain types
+                default_value = cls_dict.get(attr_name)
 
+                # check if provided default value is of correct type
+                if default_value is not None and not isinstance(default_value, annotation_type.py_type):
+                    raise TypeError(f"Struct <'{classname}'>: Default value provided for field '{attr_name}' is "
+                        f"'{str(type(default_value))}', expected '{str(type(annotation_type.py_type))}'")
 
+                format_chunks.append((attr_name, annotation_type.format, default_value, 1))
 
+        # pack format
+        format_processed = "".join(recurse_format_chunks(format_chunks))
 
-            annotations.get(attr_name)
+        cls_dict['format_chunks'] = format_chunks
+        cls_dict['write'] = StructMeta._struct_format_chunked
+        cls_dict['read'] = StructMeta.read
+        cls_dict['write'] = StructMeta.write
+
+        print(format_processed)
+
 
         return type.__new__(mcs, classname, bases, cls_dict)
+
+    def write(self, f: IOBase) -> StructIOProtocol:
+        return self
+
+    def read(self, f: IOBase) -> StructIOProtocol:
+        return self
+
+    def _struct_format_chunked(cls) -> T:
+        return cls.format_chunks
